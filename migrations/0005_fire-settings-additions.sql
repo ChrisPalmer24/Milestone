69f02ba9dd462156d8426bc7a8dae98bfcfba45a
Gary Law <skin@skinv1.co.uk>
2025-06-24 15:37:27 +0800

    fire settings additions should have migration

---
+++ migrations/0005_fire-settings-additions.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "fire_settings" ADD COLUMN "adjust_inflation" boolean DEFAULT true NOT NULL;--> statement-breakpoint
+ALTER TABLE "fire_settings" ADD COLUMN "state_pension_age" integer DEFAULT 66 NOT NULL;
